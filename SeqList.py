efc18d124fe986495ed96a08587aeba6a7e8cc8c
alvydas spokauskas <alspok@gmail.com>
2021-11-23 20:53:09 +0200

    .

--- SeqList.py
+++ SeqList.py
@@ -2,7 +2,8 @@ import os
 from OligoCalcDB import dinucIndex
 
 def seqList():
-    path = "C:\\Users\\hp\\source\\repos\\Sequencies\\Bacteria\\"
+    # path = "C:\\Users\\hp\\source\\repos\\Sequencies\\Procaryote\\"
+    path = "C:\\Users\\hp\\source\\repos\\Sequencies\\Eucaryote\\H.sapiens\\"
     files = os.listdir(path)
     
     i = 1
