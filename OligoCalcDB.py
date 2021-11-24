e415fdeb5f77819d973757fe7a11e28edf273631
alvydas spokauskas <alspok@gmail.com>
2021-11-24 12:53:11 +0200

    .

--- OligoCalcDB.py
+++ OligoCalcDB.py
@@ -5,24 +5,14 @@ from Classes.OligoCountY import OligoCountY
 from Classes.SeqMetaDB import SQLQuery, SeqMetaDB
 from Classes.OligoDict import OligoDict
 
-def dinucIndex(file_path, seq_name):
+def dinucIndex(seq: object) -> None:
 # def dinucIndex():
-    print("Dinuc calc, result to SQL")
-    # file_path = "C:\\Users\\hp\\source\\repos\\Sequencies\\"
-    # # seq_name = "Bacteria\\Escherichia.coli.fasta"
-    # # seq_name = "Bacteria\\Lactococcus.lactis.fasta"
-    # # seq_name = "Test.fasta"
-    # seq_name = "Test16.fasta"
-    # # seq_name = "TestMono.fasta"
-    
-    seq = SeqIO.read(file_path + seq_name, 'fasta')
     
     oligoDBDict = defaultdict()
     oligoDBDict.update(OligoDict().headDict(seq))
     
     dinucDict = OligoCountY(str(seq.seq)).oligoPosList()
     dinucDict = dict(sorted(dinucDict.items()))
-    # seqDinucLength = len(seq.seq) // 2
     seqDinucLength = NucCalculate(str(seq.seq)).regexSeqLength() // 2
     dinucCountStr = OligoDict().dinucDict(dinucDict, seqDinucLength)
     oligoDBDict.update(dinucCountStr)
--- SeqInput.py
+++ SeqInput.py
@@ -1,13 +1,18 @@
 import os
 from Bio import SeqIO
+from OligoCalcDB import dinucIndex
 
 def seqInput():
 
     path = "C:\\Users\\hp\\source\\repos\\Sequencies\\Temp\\genome_assemblies_genome_fasta\\ncbi-genomes-2021-11-23\\"
 
+    i = 1
     for file in os.listdir(path):
         for record in SeqIO.parse(path + file, "fasta"):
-            print(f"{record.id}\t{record.description}\t{len(record)}\t{record.seq[:50]}...{record.seq[-200:]}")
+            print(f"{i} Dinuc calc in frames.")
+            print(f"{record.id}\t{record.description}\t{len(record)}\t{record.seq[:20]}...{record.seq[-20:]}")
+            dinucIndex(record)
+            i += 1
         
     pass    
 
