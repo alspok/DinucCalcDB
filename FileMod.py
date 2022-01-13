49b499da6ce7656c5b7065f791bb9184199dda6b
alvydas <alspok@gmail.com>
2022-01-14 00:28:37 +0200

    .

---
+++ FileMod.py
@@ -0,0 +1,18 @@
+def fileMode():
+
+    input_file = "GenomeTable\\embryophyta"
+    output_file = "GenomeTable\\embryophyta.mod"
+
+    with open(input_file, "r") as fhr:
+        read_list = list(fhr)
+
+    with open(output_file, "w") as frw:
+        for line in read_list:
+            temp_line = line.split("\t")
+            frw.write(temp_line[-1])
+
+    print(f"Assembly id in file {output_file} done.")            
+    pass
+
+if __name__ == "__main__":
+    fileMode()
