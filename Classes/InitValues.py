6fa2c41698e1cf7dae6394707860dc3e7ff9ffc0
alvydas <alspok@gmail.com>
2022-02-27 20:17:25 +0200

    .

--- Classes/InitValues.py
+++ Classes/InitValues.py
@@ -1,10 +1,21 @@
-"""Class to init variables of whole project"""
-class InitValues():
 
+class InitValues():
+    """Class to init variables of whole project
+
+    Enterobacteria path and files
     path = "Genomes\\Enterobacteria\\" # data input file path
     file_name = "enterobacteria" # data input file name
     file_name_long = "enterobacteria_long" # data input file name
-    
+
+    path = "Genomes\\Plants\\"
+    file_name = "embryophyta"
+    file_name_long = "embryophyta_long"
+    """
+
+    path = "Genomes\\Procaryote"
+    file_name = "procaryote"
+    file_name_long = "procaryote_long"
+
     dbname = path + file_name + ".sqlite3" # sqlite db name with extention
     dbtable = "dinuctbl" # sqlite db table name
 
